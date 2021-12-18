52bc8c3f0ff77c8d447fa8da3571879f308f384d
Minwook Lee <dnr14@naver.com>
2021-12-19 00:01:33 +0900

    refactor : api spec이 변경됨

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -6,6 +6,8 @@ const List = ({ setCurrnetProperty, currnetProperty }) => {
     setCurrnetProperty(e.target.dataset.property);
   };
 
+  // 치료자 격리해제가 더 이상 제공을 안해주는거 같다.
+  // api가 끊이기전에 미리 포트폴리오를 만들자.
   const [lis] = useState([
     {
       name: "사망자",
@@ -21,7 +23,7 @@ const List = ({ setCurrnetProperty, currnetProperty }) => {
     },
     {
       name: "검사 진행",
-      property: "examCnt",
+      property: "accExamCnt",
     },
     {
       name: "치료자",
@@ -33,7 +35,11 @@ const List = ({ setCurrnetProperty, currnetProperty }) => {
     <StyledList>
       {lis.map((li, idx) => (
         <li key={idx}>
-          <span onClick={handleClick} className={li.property === currnetProperty ? "selected" : null} data-property={li.property}>
+          <span
+            onClick={handleClick}
+            className={li.property === currnetProperty ? "selected" : null}
+            data-property={li.property}
+          >
             {li.name}
           </span>
         </li>
